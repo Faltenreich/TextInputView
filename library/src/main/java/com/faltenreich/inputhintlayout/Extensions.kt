225e48b4de5dd191e1e2aee7d40cc70a4fc4e9d6
Faltenreich <philipp.fahlteich@gmail.com>
2018-01-23 22:06:59 +0100

    Break line before overlapping hint

--- library/src/main/java/com/faltenreich/inputhintlayout/Extensions.kt
+++ library/src/main/java/com/faltenreich/inputhintlayout/Extensions.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.util.TypedValue
 import android.view.View
 import android.view.animation.Interpolator
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -46,9 +47,24 @@ fun View.alphaAnimation(fadeIn: Boolean, durationMillis: Long, interpolator: Int
     }
 }
 
-fun View.layoutGravity(gravity: Int) {
+fun View.setLayoutGravity(gravity: Int) {
     val newParams = layoutParams
     (newParams as? FrameLayout.LayoutParams)?.gravity = gravity
     (newParams as? LinearLayout.LayoutParams)?.gravity = gravity
     layoutParams = newParams
 }
+
+fun EditText.getTextWidth(line: Int = -1): Float {
+    val input = if (line >= 0) getTextForLine(line) else text?.toString()
+    return input?.let { paint.measureText(input) } ?: 0f
+}
+
+fun EditText.getTextForLine(line: Int): String? =
+        layout?.let {
+            val input = text?.toString()
+            val lineCharacters = input?.length ?: 0
+            val start = it.getLineStart(line)
+            val end = it.getLineEnd(line)
+            val isInBounds = start < end && start < lineCharacters && end <= lineCharacters
+            if (isInBounds) input?.substring(start, end) else null
+        }
--- library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
+++ library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
@@ -28,7 +28,6 @@ class InputHintLayout @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     private var customAnimationDuration: Long = 0
-    private var customGravity: Int = 0
     private var customTextSize: Float = 0f
     private var customTextColorNormal: Int = 0
     private var customTextColorSelected: Int = 0
@@ -43,6 +42,9 @@ class InputHintLayout @JvmOverloads constructor(
         hintView
     }
 
+    private var maxLineWidth: Int = 0
+        get() = editText.width - hintView.width - hintView.paddingLeft - hintView.paddingRight
+
     private var textColor: Int
         get() = hintView.textColors.defaultColor
         set(value) { hintView.setTextColor(value, animationDurationMillis, interpolator) }
@@ -51,10 +53,6 @@ class InputHintLayout @JvmOverloads constructor(
 
     var interpolator: Interpolator = AccelerateDecelerateInterpolator()
 
-    var gravity: Int
-        get() = (hintView.layoutParams as FrameLayout.LayoutParams).gravity
-        set(value) { hintView.layoutGravity(value) }
-
     var textSize: Float
         get() = hintView.textSize
         set(value) { hintView.setTextSize(TypedValue.COMPLEX_UNIT_PX, editText.textSize) }
@@ -62,20 +60,19 @@ class InputHintLayout @JvmOverloads constructor(
     var textColorNormal: Int = -1
         set(value) {
             field = value
-            invalidateHintTextColor()
+            invalidateHint()
         }
 
     var textColorSelected: Int = -1
         set(value) {
             field = value
-            invalidateHintTextColor()
+            invalidateHint()
         }
 
     init {
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.InputHintLayout, 0, 0)
             customAnimationDuration = typedArray.getInt(R.styleable.InputHintLayout_animationDurationMillis, -1).toLong()
-            customGravity = typedArray.getInt(R.styleable.InputHintLayout_android_gravity, -1)
             customTextSize = typedArray.getFloat(R.styleable.InputHintLayout_android_textSize, -1f)
             customTextColorNormal = typedArray.getColorStateList(R.styleable.InputHintLayout_android_textColor)?.defaultColor ?: -1
             customTextColorSelected = typedArray.getColorStateList(R.styleable.InputHintLayout_android_tint)?.defaultColor ?: -1
@@ -89,39 +86,46 @@ class InputHintLayout @JvmOverloads constructor(
     }
 
     private fun initLayout() {
+        editText.gravity= Gravity.TOP
+
         hintView.setPadding(editText.paddingLeft, editText.paddingTop, editText.paddingRight, editText.paddingBottom)
+        hintView.setLayoutGravity(Gravity.BOTTOM or Gravity.END) // TODO: Rtl
         hintView.text = editText.hint
 
         animationDurationMillis = if (customAnimationDuration >= 0) customAnimationDuration else ANIMATION_DURATION_DEFAULT
-        gravity = if (customGravity >= 0) customGravity else Gravity.END
         textSize = if (customTextSize >= 0) customTextSize else editText.textSize
         textColorNormal = if (customTextColorNormal >= 0) customTextColorNormal else editText.hintTextColors.defaultColor
         textColorSelected = if (customTextColorSelected >= 0) customTextColorSelected else context.accentColor()
 
-        // TODO: Multiline Gravity
         // TODO: Transition
 
         if (!isInEditMode) {
             editText.addTextChangedListener(object: TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
                 override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) { invalidateHintVisibility() }
+                override fun afterTextChanged(p0: Editable?) { invalidateHint() }
             })
 
-            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> invalidateHintTextColor() }
+            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> invalidateHint() }
 
             val showHint = editText.text.isNotEmpty()
             hintView.alpha = if (showHint) 1f else 0f
         }
     }
 
-    private fun invalidateHintTextColor() {
+    private fun invalidateHint() {
         textColor = if (editText.hasFocus()) textColorSelected else textColorNormal
-    }
 
-    private fun invalidateHintVisibility() {
         val showHint = editText.text.isNotEmpty()
         hintView.alphaAnimation(showHint, animationDurationMillis, interpolator)
+
+        val currentLineWidth = editText.getTextWidth(editText.lineCount - 1)
+        val exceededLineWidth = currentLineWidth > maxLineWidth
+        if (exceededLineWidth) {
+            // TODO: Regard maxLines
+            // FIXME: setLines() on exceeding line
+            editText.setLines(editText.lineCount + 1)
+        }
     }
 
     private val ViewGroup.views: List<View>
