785d4e206ae519561d27feb24e62de8f3da5e146
Faltenreich <philipp.fahlteich@gmail.com>
2018-01-26 22:18:27 +0100

    Added enum for overlap animations

--- library/src/main/java/com/faltenreich/inputhintlayout/Extensions.kt
+++ library/src/main/java/com/faltenreich/inputhintlayout/Extensions.kt
@@ -18,7 +18,7 @@ import android.widget.TextView
  * Created by Faltenreich on 22.01.2018
  */
 
-fun Any.tag() = javaClass.simpleName
+fun Any.tag(): String = javaClass.simpleName
 
 fun Context.accentColor(): Int {
     val attr =
@@ -44,15 +44,6 @@ fun TextView.setTextColor(colorTo: Int, durationMillis: Long, interpolator: Inte
     }
 }
 
-fun View.alphaAnimation(fadeIn: Boolean, durationMillis: Long, interpolator: Interpolator) {
-    val target = if (fadeIn) 1f else 0f
-    if (durationMillis > 0 && target != alpha) {
-        animate().alpha(target).setDuration(durationMillis).setInterpolator(interpolator).start()
-    } else {
-        alpha = target
-    }
-}
-
 fun View.setLayoutGravity(gravity: Int) {
     val newParams = layoutParams
     (newParams as? FrameLayout.LayoutParams)?.gravity = gravity
@@ -75,44 +66,32 @@ fun EditText.getTextForLine(line: Int): String? =
             if (isInBounds) input?.substring(start, end) else null
         }
 
-fun EditText.getTextWidth(range: IntRange): Float {
-    val input = getTextForRange(range)
-    return input?.let { paint.measureText(input) } ?: 0f
-}
+fun EditText.getMaxLineCountCompat(): Int =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) maxLines else getMaxLineCountPreApi16()
 
-fun EditText.getTextForRange(range: IntRange): String? =
-        layout?.let {
-            val input = text?.toString()
-            val lineCharacters = input?.length ?: 0
-            val start = range.start
-            val end = range.endInclusive
-            val isInBounds = start < end && start < lineCharacters && end <= lineCharacters
-            if (isInBounds) input?.substring(start, end) else null
+fun EditText.getMaxLineCountPreApi16(): Int =
+        try {
+            val maximumField = text.javaClass.getDeclaredField("mMaximum")
+            val maxModeField = text.javaClass.getDeclaredField("mMaxMode")
+            maximumField?.isAccessible = true
+            maxModeField?.isAccessible = true
+            val maximum = maximumField?.getInt(text)
+            val maxMode = maxModeField?.getInt(text)
+            if (maxMode == 1) 1 else maximum ?: 1
+        } catch (exception: Exception) {
+            Log.e(tag(), exception.message)
+            1
         }
 
-fun EditText.getMaxLineCountCompat(): Int {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) maxLines else getMaxLineCountPreApi16()
-}
-
-fun EditText.getMaxLineCountPreApi16(): Int {
-    try {
-        val maximumField = text.javaClass.getDeclaredField("mMaximum")
-        val maxModeField = text.javaClass.getDeclaredField("mMaxMode")
-
-        maximumField?.isAccessible = true
-        maxModeField?.isAccessible = true
-
-        val maximum = maximumField?.getInt(text)
-        val maxMode = maxModeField?.getInt(text)
-
-        return if (maxMode == 1) 1 else maximum ?: 1
-
-    } catch (exception: NoSuchFieldException) {
-        Log.e(tag(), exception.message)
-    } catch (exception: IllegalArgumentException) {
-        Log.e(tag(), exception.message)
-    } catch (exception: IllegalAccessException) {
-        Log.e(tag(), exception.message)
+fun View.setOffsetStart(offset: Float) {
+    val newParams = layoutParams
+    val margin = (-offset).toInt()
+    (newParams as? ViewGroup.MarginLayoutParams?)?.let {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            it.marginEnd = margin
+        } else {
+            it.rightMargin = margin
+        }
+        layoutParams = it
     }
-    return 1
 }
--- library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
+++ library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
@@ -21,7 +21,16 @@ import android.widget.TextView
  * Created by Faltenreich on 21.01.2018
  */
 
+const val OVERLAP_ANIMATION_TOGGLE = 0
+const val OVERLAP_ANIMATION_FADE = 1
+const val OVERLAP_ANIMATION_PUSH = 2
+
+const val HIDE_ANIMATION_STATUS_IDLE = 0
+const val HIDE_ANIMATION_STATUS_ANIMATING_IN = 1
+const val HIDE_ANIMATION_STATUS_ANIMATION_OUT = 2
+
 private const val ANIMATION_DURATION_DEFAULT = 300L
+private const val OVERLAP_ANIMATION_DEFAULT = OVERLAP_ANIMATION_TOGGLE
 
 @Suppress("MemberVisibilityCanBePrivate")
 class InputHintLayout @JvmOverloads constructor(
@@ -30,19 +39,14 @@ class InputHintLayout @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private enum class Status {
-        IDLE,
-        ANIMATING_IN,
-        ANIMATING_OUT
-    }
+    private var customOverlapAnimation: Int = -1
+    private var customAnimationDuration: Long = -1
+    private var customTextSize: Float = -1f
+    private var customTextColorNormal: Int = -1
+    private var customTextColorSelected: Int = -1
 
-    private var customAnimationDuration: Long = 0
-    private var customTextSize: Float = 0f
-    private var customTextColorNormal: Int = 0
-    private var customTextColorSelected: Int = 0
-
-    private var currentStatus: Status = Status.IDLE
-    private var pendingStatus: Status = Status.IDLE
+    private var currentAnimationStatus = HIDE_ANIMATION_STATUS_IDLE
+    private var pendingAnimationStatus = HIDE_ANIMATION_STATUS_IDLE
 
     private var maxLineCount: Int = 1
 
@@ -64,7 +68,9 @@ class InputHintLayout @JvmOverloads constructor(
         get() = hintView.textColors.defaultColor
         set(value) { hintView.setTextColor(value, animationDurationMillis, interpolator) }
 
-    var animationDurationMillis: Long = ANIMATION_DURATION_DEFAULT
+    var overlapAnimation: Int = -1
+
+    var animationDurationMillis: Long = -1L
 
     var interpolator: Interpolator = AccelerateDecelerateInterpolator()
 
@@ -75,18 +81,20 @@ class InputHintLayout @JvmOverloads constructor(
     var textColorNormal: Int = -1
         set(value) {
             field = value
-            invalidateHintColor()
+            onInputFocusChanged()
         }
 
     var textColorSelected: Int = -1
         set(value) {
             field = value
-            invalidateHintColor()
+            onInputFocusChanged()
         }
 
     init {
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.InputHintLayout, 0, 0)
+
+            customOverlapAnimation = typedArray.getInt(R.styleable.InputHintLayout_overlapAnimation, -1)
             customAnimationDuration = typedArray.getInt(R.styleable.InputHintLayout_animationDurationMillis, -1).toLong()
             customTextSize = typedArray.getFloat(R.styleable.InputHintLayout_android_textSize, -1f)
             customTextColorNormal = typedArray.getColorStateList(R.styleable.InputHintLayout_android_textColor)?.defaultColor ?: -1
@@ -101,9 +109,14 @@ class InputHintLayout @JvmOverloads constructor(
     }
 
     private fun initLayout() {
+        if (editText.gravity != Gravity.TOP) {
+            Log.d(tag(), "Gravity gets overwritten to Gravity.TOP in order to support multiline text")
+        }
+
         editText.gravity = Gravity.TOP
         maxLineCount = editText.getMaxLineCountCompat()
 
+        overlapAnimation = if (customOverlapAnimation >= 0) customOverlapAnimation else OVERLAP_ANIMATION_DEFAULT
         animationDurationMillis = if (customAnimationDuration >= 0) customAnimationDuration else ANIMATION_DURATION_DEFAULT
         textSize = if (customTextSize >= 0) customTextSize else editText.textSize
         textColorNormal = if (customTextColorNormal >= 0) customTextColorNormal else editText.hintTextColors.defaultColor
@@ -113,85 +126,24 @@ class InputHintLayout @JvmOverloads constructor(
             editText.addTextChangedListener(object: TextWatcher {
                 override fun beforeTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {}
                 override fun onTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {
-                    invalidateHintPosition()
+                    onInputTextChanged()
 
                     val firstWord = start == 0
                     val inBounds = text?.length ?: 0 <= 1
                     val emptied = count == 1 && after == 0
                     val unemptied = count == 0 && after == 1
                     if (firstWord && inBounds && (emptied || unemptied)) {
-                        toggleHintVisibility(after == 1)
+                        onInputTextIsEmptyChanged(after == 1)
                     }
                 }
                 override fun afterTextChanged(editable: Editable?) {}
             })
 
-            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> invalidateHintColor() }
+            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> onInputFocusChanged() }
 
             val showHint = editText.text.isNotEmpty()
             hintView.alpha = if (showHint) 1f else 0f
         }
-    }
-
-    private fun invalidateHintColor() {
-        textColor = if (editText.hasFocus()) textColorSelected else textColorNormal
-    }
-
-    private fun invalidateHintPosition() {
-        // Ensure correct line count on shrinking text
-        if (editText.lineCount <= maxLineCount) {
-            editText.setLines(editText.lineCount)
-        }
-
-        val currentLineWidth = editText.getTextWidth(editText.lineCount - 1)
-        val exceededLineWidth = currentLineWidth > maxLineWidth
-        if (exceededLineWidth) {
-            val reachedEnd = editText.lineCount >= maxLineCount
-            if (reachedEnd) {
-                // TODO: Ellipsize early
-                hintView.visibility = View.INVISIBLE
-            } else {
-                editText.setLines(editText.lineCount + 1)
-            }
-        } else {
-            hintView.visibility = View.VISIBLE
-        }
-    }
-
-    private fun toggleHintVisibility(isVisible: Boolean) {
-        pendingStatus = Status.IDLE
-        when (currentStatus) {
-            Status.IDLE -> animateHintVisibility(isVisible)
-            Status.ANIMATING_IN -> if (!isVisible) pendingStatus = Status.ANIMATING_OUT
-            Status.ANIMATING_OUT -> { if (isVisible) pendingStatus = Status.ANIMATING_IN }
-        }
-    }
-
-    private fun animateHintVisibility(animateIn: Boolean, animator: ViewPropertyAnimator = if (animateIn) onCreateInAnimation(hintView) else onCreateOutAnimation(hintView)) {
-        animator.duration = animationDurationMillis
-        animator.interpolator = interpolator
-        animator.setListener(object: Animator.AnimatorListener {
-            override fun onAnimationStart(animator: Animator?) {
-                currentStatus = if (animateIn) Status.ANIMATING_IN else Status.ANIMATING_OUT
-                Log.d(tag(), "Started animation $currentStatus")
-            }
-            override fun onAnimationEnd(animator: Animator?) {
-                Log.d(tag(), "Finished animation $currentStatus")
-                currentStatus = Status.IDLE
-                when (pendingStatus) {
-                    Status.ANIMATING_IN -> toggleHintVisibility(true)
-                    Status.ANIMATING_OUT -> toggleHintVisibility(false)
-                    else -> Unit
-                }
-            }
-            override fun onAnimationCancel(animator: Animator?) {
-                currentStatus = Status.IDLE
-            }
-            override fun onAnimationRepeat(animator: Animator?) {
-                currentStatus = if (animateIn) Status.ANIMATING_IN else Status.ANIMATING_OUT
-            }
-        })
-        animator.start()
     }
 
     private fun onAttachEditText(): EditText =
@@ -215,4 +167,81 @@ class InputHintLayout @JvmOverloads constructor(
     private fun onCreateOutAlphaAnimation(view: View): ViewPropertyAnimator = view.animate().alpha(0f)
 
     fun onCreateOutAnimation(view: View): ViewPropertyAnimator = onCreateOutAlphaAnimation(view)
+
+    private fun onInputFocusChanged() {
+        textColor = if (editText.hasFocus()) textColorSelected else textColorNormal
+    }
+
+    private fun onInputTextChanged() {
+        // Ensure correct line count on shrinking text
+        if (editText.lineCount <= maxLineCount) {
+            editText.setLines(editText.lineCount)
+        }
+
+        val currentLineWidth = editText.getTextWidth(editText.lineCount - 1)
+        val exceededLineWidth = currentLineWidth > maxLineWidth
+        if (exceededLineWidth) {
+            if (editText.lineCount < maxLineCount) {
+                editText.setLines(editText.lineCount + 1)
+            } else {
+                onInputTextChangedOutOfBounds(currentLineWidth - maxLineWidth)
+            }
+        } else {
+            onInputTextChangedInBounds()
+        }
+    }
+
+    fun onInputTextChangedOutOfBounds(offset: Float) {
+        when (overlapAnimation) {
+            OVERLAP_ANIMATION_TOGGLE -> hintView.visibility = View.INVISIBLE
+            OVERLAP_ANIMATION_FADE -> hintView.visibility = View.INVISIBLE // TODO
+            OVERLAP_ANIMATION_PUSH -> hintView.setOffsetStart(offset)
+        }
+    }
+
+    fun onInputTextChangedInBounds() {
+        when (overlapAnimation) {
+            OVERLAP_ANIMATION_TOGGLE -> hintView.visibility = View.VISIBLE
+            OVERLAP_ANIMATION_FADE -> hintView.visibility = View.VISIBLE // TODO
+            OVERLAP_ANIMATION_PUSH -> hintView.setOffsetStart(0f)
+        }
+    }
+
+    private fun onInputTextIsEmptyChanged(isVisible: Boolean) {
+        pendingAnimationStatus = HIDE_ANIMATION_STATUS_IDLE
+        when (currentAnimationStatus) {
+            HIDE_ANIMATION_STATUS_IDLE -> onInputVisibilityChange(isVisible)
+            HIDE_ANIMATION_STATUS_ANIMATING_IN -> if (!isVisible) pendingAnimationStatus = HIDE_ANIMATION_STATUS_ANIMATION_OUT
+            HIDE_ANIMATION_STATUS_ANIMATION_OUT -> { if (isVisible) pendingAnimationStatus = HIDE_ANIMATION_STATUS_ANIMATING_IN }
+        }
+    }
+
+    private fun onInputVisibilityChange(
+            animateIn: Boolean,
+            animator: ViewPropertyAnimator = if (animateIn) onCreateInAnimation(hintView) else onCreateOutAnimation(hintView)) {
+        animator.duration = animationDurationMillis
+        animator.interpolator = interpolator
+        animator.setListener(object: Animator.AnimatorListener {
+            override fun onAnimationStart(animator: Animator?) {
+                currentAnimationStatus = if (animateIn) HIDE_ANIMATION_STATUS_ANIMATING_IN else HIDE_ANIMATION_STATUS_ANIMATION_OUT
+                Log.d(tag(), "Started animation $currentAnimationStatus")
+            }
+            override fun onAnimationEnd(animator: Animator?) {
+                Log.d(tag(), "Finished animation $currentAnimationStatus")
+                currentAnimationStatus = HIDE_ANIMATION_STATUS_IDLE
+                when (pendingAnimationStatus) {
+                    HIDE_ANIMATION_STATUS_ANIMATING_IN -> onInputTextIsEmptyChanged(true)
+                    HIDE_ANIMATION_STATUS_ANIMATION_OUT -> onInputTextIsEmptyChanged(false)
+                    else -> Unit
+                }
+            }
+            override fun onAnimationCancel(animator: Animator?) {
+                currentAnimationStatus = HIDE_ANIMATION_STATUS_IDLE
+            }
+            override fun onAnimationRepeat(animator: Animator?) {
+                currentAnimationStatus = if (animateIn) HIDE_ANIMATION_STATUS_ANIMATING_IN else HIDE_ANIMATION_STATUS_ANIMATION_OUT
+            }
+        })
+        animator.start()
+    }
 }
