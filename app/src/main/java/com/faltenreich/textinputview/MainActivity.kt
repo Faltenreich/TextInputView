6fec9ce9488b3ea9ec0e562229f2e8c20dad78b7
Faltenreich <philipp.fahlteich@gmail.com>
2018-02-11 21:20:14 +0100

    Support programmatically generated views

--- app/src/main/java/com/faltenreich/textinputview/MainActivity.kt
+++ app/src/main/java/com/faltenreich/textinputview/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import android.os.Message
 import android.support.v7.app.AppCompatActivity
 import android.text.InputType
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -19,7 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private fun addTextInputView() {
+        val editText = EditText(this)
+        editText.maxLines = 2
+        editText.setText("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua")
+        editText.hint = "Hint"
+        val textInputView = TextInputView(this, editText = editText)
+        // FIXME: This leads to broken lines
+        layout.addView(textInputView)
+    }
+
     private fun showcase() {
+        editText.clearFocus()
+
         val input = "Input"
         val delay = 5000L
         val speed = 200L
--- library/src/main/java/com/faltenreich/textinputview/TextInputView.kt
+++ library/src/main/java/com/faltenreich/textinputview/TextInputView.kt
@@ -18,28 +18,38 @@ import android.widget.TextView
  * Created by Faltenreich on 21.01.2018
  */
 
-const val ANIMATION_STYLE_TOGGLE = 0
-const val ANIMATION_STYLE_PUSH = 1
+const val OVERLAP_ACTION_TOGGLE = 0
+const val OVERLAP_ACTION_PUSH = 1
 
 private const val ANIMATION_DURATION = 200L
 
 @Suppress("MemberVisibilityCanBePrivate")
-open class TextInputView @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+open class TextInputView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, editText: EditText? = null) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private var customOverlapAnimation: Int = -1
+    private var customOverlapAction: Int = -1
     private var customTextSize: Float = -1f
     private var customTextColorNormal: Int = -1
     private var customTextColorSelected: Int = -1
 
     private var maxLineCount: Int = 1
 
-    private val editText: EditText by lazy { onAttachEditText() }
+    private val editText: EditText by lazy {
+        try { views().first { it is EditText } as EditText }
+        catch (exception: NoSuchElementException) { throw Exception("${tag()} requires an EditText as first child") } }
 
-    private val hintView: TextView by lazy { onCreateHintView() }
+    private val hintView: TextView by lazy {
+        val hintView = TextView(context)
+        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        hintView.layoutParams = layoutParams
+
+        // Replace the original hint of the wrapped EditText
+        hintView.setPadding(this.editText.paddingLeft, this.editText.paddingTop, this.editText.paddingRight, this.editText.paddingBottom)
+        hintView.text = this.editText.hint
+        this.editText.hint = null
+
+        addView(hintView)
+        hintView
+    }
 
     private var maxLineWidth: Int = 0
         get() = editText.width - hintView.width
@@ -48,7 +58,7 @@ open class TextInputView @JvmOverloads constructor(
         get() = hintView.textColors.defaultColor
         set(value) { hintView.setTextColor(value, ANIMATION_DURATION, interpolator) }
 
-    var overlapAnimation: Int = -1
+    var overlapAction: Int = -1
 
     var interpolator: Interpolator = AccelerateDecelerateInterpolator()
 
@@ -71,12 +81,13 @@ open class TextInputView @JvmOverloads constructor(
     init {
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.TextInputView, 0, 0)
-            customOverlapAnimation = typedArray.getInt(R.styleable.TextInputView_overlapAnimation, -1)
+            customOverlapAction = typedArray.getInt(R.styleable.TextInputView_overlapAction, -1)
             customTextSize = typedArray.getFloat(R.styleable.TextInputView_android_textSize, -1f)
             customTextColorNormal = typedArray.getColorStateList(R.styleable.TextInputView_android_textColor)?.defaultColor ?: -1
             customTextColorSelected = typedArray.getColorStateList(R.styleable.TextInputView_android_tint)?.defaultColor ?: -1
             typedArray.recycle()
         }
+        editText?.let { addView(editText) }
     }
 
     override fun onAttachedToWindow() {
@@ -93,7 +104,7 @@ open class TextInputView @JvmOverloads constructor(
             hintView.setLayoutGravity(Gravity.BOTTOM)
         }
 
-        overlapAnimation = if (customOverlapAnimation >= 0) customOverlapAnimation else ANIMATION_STYLE_TOGGLE
+        overlapAction = if (customOverlapAction >= 0) customOverlapAction else OVERLAP_ACTION_TOGGLE
         textSize = if (customTextSize >= 0) customTextSize else editText.textSize
         textColorNormal = if (customTextColorNormal >= 0) customTextColorNormal else editText.hintTextColors.defaultColor
         textColorSelected = if (customTextColorSelected >= 0) customTextColorSelected else context.accentColor()
@@ -105,28 +116,8 @@ open class TextInputView @JvmOverloads constructor(
                 override fun onTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) { onInputTextChanged() }
                 override fun afterTextChanged(editable: Editable?) {}
             })
+            onInputTextChanged()
         }
-    }
-
-    private fun onAttachEditText(): EditText =
-            try {
-                views().first { it is EditText } as EditText
-            } catch (exception: NoSuchElementException) {
-                throw Exception("${tag()} requires an EditText as first child")
-            }
-
-    private fun onCreateHintView(): TextView {
-        val hintView = TextView(context)
-        val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        hintView.layoutParams = layoutParams
-
-        // Replace the original hint of the wrapped EditText
-        hintView.setPadding(editText.paddingLeft, editText.paddingTop, editText.paddingRight, editText.paddingBottom)
-        hintView.text = editText.hint
-        editText.hint = null
-
-        addView(hintView)
-        return hintView
     }
 
     private fun onInputFocusChanged() {
@@ -160,9 +151,9 @@ open class TextInputView @JvmOverloads constructor(
 
     private fun onInputTextOffsetChanged(offset: Float) {
         val overlaps = offset > 0
-        when (overlapAnimation) {
-            ANIMATION_STYLE_TOGGLE -> hintView.visibility = if (overlaps) View.INVISIBLE else View.VISIBLE
-            ANIMATION_STYLE_PUSH -> hintView.setOffsetStart(offset)
+        when (overlapAction) {
+            OVERLAP_ACTION_TOGGLE -> hintView.visibility = if (overlaps) View.INVISIBLE else View.VISIBLE
+            OVERLAP_ACTION_PUSH -> hintView.setOffsetStart(offset)
         }
     }
 }
