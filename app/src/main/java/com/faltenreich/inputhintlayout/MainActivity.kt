2317eef2af8cceeb9715f4eb8c286e0b999bfc9a
Faltenreich <philipp.fahlteich@gmail.com>
2018-02-11 19:26:39 +0100

    Animated hint onFocusChanged Simplified view

--- app/src/main/java/com/faltenreich/inputhintlayout/MainActivity.kt
+++ app/src/main/java/com/faltenreich/inputhintlayout/MainActivity.kt
@@ -1,18 +1,63 @@
 package com.faltenreich.inputhintlayout
 
 import android.os.Bundle
-import android.support.v4.widget.TextViewCompat
+import android.os.Handler
+import android.os.Message
 import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
+import android.text.InputType
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 /**
  * Created by Faltenreich on 21.01.2018
  */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TextViewCompat.getMaxLines(EditText(this))
+    }
+
+    private fun showcase() {
+        val input = "Input"
+        val delay = 5000L
+        val speed = 200L
+
+        var add = true
+        var index = 0
+
+        editText.inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+
+        val handler = Handler { message ->
+            editText.setText(message.obj as String)
+            editText.setSelection(editText.text.length);
+            true
+        }
+
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object: TimerTask() {
+            override fun run() {
+                val message = Message()
+                message.obj = if (index >= 0) input.substring(IntRange(0, index)) else ""
+                handler.sendMessage(message)
+
+                if (add) {
+                    if (index < input.length - 1) {
+                        index++
+                    } else {
+                        add = !add
+                        index--
+                    }
+                } else {
+                    if (index < 0) {
+                        timer.cancel()
+                        timer.purge()
+                    } else {
+                        index--
+                    }
+                }
+            }
+        }, delay, speed)
     }
 }
--- library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
+++ library/src/main/java/com/faltenreich/inputhintlayout/InputHintLayout.kt
@@ -1,16 +1,13 @@
 package com.faltenreich.inputhintlayout
 
-import android.animation.Animator
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
 import android.view.View.OnFocusChangeListener
-import android.view.ViewPropertyAnimator
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.Interpolator
 import android.widget.EditText
@@ -22,16 +19,9 @@ import android.widget.TextView
  */
 
 const val ANIMATION_STYLE_TOGGLE = 0
-const val ANIMATION_STYLE_ANIMATE = 1
-const val ANIMATION_STYLE_PUSH = 2
+const val ANIMATION_STYLE_PUSH = 1
 
-private const val ANIMATION_STATUS_IDLE = 0
-private const val ANIMATION_STATUS_ANIMATING_IN = 1
-private const val ANIMATION_STATUS_ANIMATING_OUT = 2
-
-private const val ANIMATION_DURATION_DEFAULT = 300L
-private const val MOVE_ANIMATION_DEFAULT = ANIMATION_STYLE_ANIMATE
-private const val OVERLAP_ANIMATION_DEFAULT = ANIMATION_STYLE_TOGGLE
+private const val ANIMATION_DURATION = 200L
 
 @Suppress("MemberVisibilityCanBePrivate")
 open class InputHintLayout @JvmOverloads constructor(
@@ -40,41 +30,25 @@ open class InputHintLayout @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private var customMoveAnimation: Int = -1
     private var customOverlapAnimation: Int = -1
-    private var customAnimationDuration: Long = -1
     private var customTextSize: Float = -1f
     private var customTextColorNormal: Int = -1
     private var customTextColorSelected: Int = -1
-
-    private var currentAnimationStatus = ANIMATION_STATUS_IDLE
-    private var pendingAnimationStatus = ANIMATION_STATUS_IDLE
 
     private var maxLineCount: Int = 1
 
     private val editText: EditText by lazy { onAttachEditText() }
 
-    private val hintView: TextView by lazy {
-        val hintView = onCreateHintView()
-        addView(hintView)
-        hintView.setPadding(editText.paddingLeft, editText.paddingTop, editText.paddingRight, editText.paddingBottom)
-        hintView.setLayoutGravity(Gravity.BOTTOM or Gravity.END) // TODO: Rtl
-        hintView.text = editText.hint
-        hintView
-    }
+    private val hintView: TextView by lazy { onCreateHintView() }
 
     private var maxLineWidth: Int = 0
-        get() = editText.width - hintView.width - hintView.paddingLeft - hintView.paddingRight
+        get() = editText.width - hintView.width
 
     private var textColor: Int
         get() = hintView.textColors.defaultColor
-        set(value) { hintView.setTextColor(value, animationDurationMillis, interpolator) }
-
-    var moveAnimation: Int = -1
+        set(value) { hintView.setTextColor(value, ANIMATION_DURATION, interpolator) }
 
     var overlapAnimation: Int = -1
-
-    var animationDurationMillis: Long = -1L
 
     var interpolator: Interpolator = AccelerateDecelerateInterpolator()
 
@@ -97,10 +71,7 @@ open class InputHintLayout @JvmOverloads constructor(
     init {
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.InputHintLayout, 0, 0)
-
-            customMoveAnimation = typedArray.getInt(R.styleable.InputHintLayout_moveAnimation, -1)
             customOverlapAnimation = typedArray.getInt(R.styleable.InputHintLayout_overlapAnimation, -1)
-            customAnimationDuration = typedArray.getInt(R.styleable.InputHintLayout_animationDurationMillis, -1).toLong()
             customTextSize = typedArray.getFloat(R.styleable.InputHintLayout_android_textSize, -1f)
             customTextColorNormal = typedArray.getColorStateList(R.styleable.InputHintLayout_android_textColor)?.defaultColor ?: -1
             customTextColorSelected = typedArray.getColorStateList(R.styleable.InputHintLayout_android_tint)?.defaultColor ?: -1
@@ -114,46 +85,26 @@ open class InputHintLayout @JvmOverloads constructor(
     }
 
     private fun initLayout() {
-        if (editText.gravity != Gravity.TOP) {
-            Log.d(tag(), "Gravity gets overwritten to Gravity.TOP in order to support multiline text")
-        }
-
         maxLineCount = editText.getMaxLineCountCompat()
 
         // Ensure input alignment on multiline
         if (maxLineCount > 1) {
             editText.gravity = Gravity.TOP
+            hintView.setLayoutGravity(Gravity.BOTTOM)
         }
 
-        moveAnimation = if (customMoveAnimation >= 0) customMoveAnimation else MOVE_ANIMATION_DEFAULT
-        overlapAnimation = if (customOverlapAnimation >= 0) customOverlapAnimation else OVERLAP_ANIMATION_DEFAULT
-        animationDurationMillis = if (customAnimationDuration >= 0) customAnimationDuration else ANIMATION_DURATION_DEFAULT
+        overlapAnimation = if (customOverlapAnimation >= 0) customOverlapAnimation else ANIMATION_STYLE_TOGGLE
         textSize = if (customTextSize >= 0) customTextSize else editText.textSize
         textColorNormal = if (customTextColorNormal >= 0) customTextColorNormal else editText.hintTextColors.defaultColor
         textColorSelected = if (customTextColorSelected >= 0) customTextColorSelected else context.accentColor()
 
         if (!isInEditMode) {
-            editText.addTextChangedListener(object: TextWatcher {
+            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> onInputFocusChanged() }
+            editText.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {}
-
-                override fun onTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {
-                    onInputTextChanged()
-
-                    val firstWord = start == 0
-                    val inBounds = text?.length ?: 0 <= 1
-                    val emptied = count == 1 && after == 0
-                    val unemptied = count == 0 && after == 1
-                    if (firstWord && inBounds && (emptied || unemptied)) {
-                        onInputTextIsEmptyChanged(after == 1)
-                    }
-                }
-
+                override fun onTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) { onInputTextChanged() }
                 override fun afterTextChanged(editable: Editable?) {}
             })
-
-            editText.onFocusChangeListener = OnFocusChangeListener { _, _ -> onInputFocusChanged() }
-
-            hintView.alpha = if (editText.text.isNotEmpty()) 1f else 0f
         }
     }
 
@@ -165,18 +116,27 @@ open class InputHintLayout @JvmOverloads constructor(
             }
 
     private fun onCreateHintView(): TextView {
-        val hintView = InputHintView(context)
+        val hintView = TextView(context)
         val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         hintView.layoutParams = layoutParams
+
+        // Replace the original hint of the wrapped EditText
+        hintView.setPadding(editText.paddingLeft, editText.paddingTop, editText.paddingRight, editText.paddingBottom)
+        hintView.text = editText.hint
+        editText.hint = null
+
+        addView(hintView)
         return hintView
     }
 
-    open fun onCreateInAnimation(view: View): ViewPropertyAnimator = view.animate().alpha(1f)
-
-    open fun onCreateOutAnimation(view: View): ViewPropertyAnimator = view.animate().alpha(0f)
-
     private fun onInputFocusChanged() {
-        textColor = if (editText.hasFocus()) textColorSelected else textColorNormal
+        val hasFocus = editText.hasFocus()
+        val isEmpty = editText.text.isEmpty()
+
+        textColor = if (hasFocus) textColorSelected else textColorNormal
+
+        val position = if (!hasFocus && isEmpty) 0 else maxLineWidth
+        hintView.animate().translationX(position.toFloat()).setDuration(ANIMATION_DURATION).start()
     }
 
     private fun onInputTextChanged() {
@@ -202,53 +162,7 @@ open class InputHintLayout @JvmOverloads constructor(
         val overlaps = offset > 0
         when (overlapAnimation) {
             ANIMATION_STYLE_TOGGLE -> hintView.visibility = if (overlaps) View.INVISIBLE else View.VISIBLE
-            ANIMATION_STYLE_ANIMATE -> onInputAnimateWhenReady(!overlaps)
             ANIMATION_STYLE_PUSH -> hintView.setOffsetStart(offset)
         }
-    }
-
-    private fun onInputTextIsEmptyChanged(isVisible: Boolean) {
-        when (moveAnimation) {
-            ANIMATION_STYLE_TOGGLE -> hintView.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
-            ANIMATION_STYLE_ANIMATE -> onInputAnimateWhenReady(isVisible)
-        }
-    }
-
-    private fun onInputAnimateWhenReady(animateIn: Boolean) {
-        pendingAnimationStatus = ANIMATION_STATUS_IDLE
-        when (currentAnimationStatus) {
-            ANIMATION_STATUS_IDLE -> onInputAnimate(animateIn)
-            ANIMATION_STATUS_ANIMATING_IN -> if (!animateIn) pendingAnimationStatus = ANIMATION_STATUS_ANIMATING_OUT
-            ANIMATION_STATUS_ANIMATING_OUT -> if (animateIn) pendingAnimationStatus = ANIMATION_STATUS_ANIMATING_IN
-        }
-    }
-
-    private fun onInputAnimate(
-            animateIn: Boolean,
-            animator: ViewPropertyAnimator = if (animateIn) onCreateInAnimation(hintView) else onCreateOutAnimation(hintView)) {
-        animator.duration = animationDurationMillis
-        animator.interpolator = interpolator
-        animator.setListener(object: Animator.AnimatorListener {
-            override fun onAnimationStart(animator: Animator?) {
-                currentAnimationStatus = if (animateIn) ANIMATION_STATUS_ANIMATING_IN else ANIMATION_STATUS_ANIMATING_OUT
-                Log.d(tag(), "Started animation $currentAnimationStatus")
-            }
-            override fun onAnimationEnd(animator: Animator?) {
-                Log.d(tag(), "Finished animation $currentAnimationStatus")
-                currentAnimationStatus = ANIMATION_STATUS_IDLE
-                when (pendingAnimationStatus) {
-                    ANIMATION_STATUS_ANIMATING_IN -> onInputTextIsEmptyChanged(true)
-                    ANIMATION_STATUS_ANIMATING_OUT -> onInputTextIsEmptyChanged(false)
-                    else -> Unit
-                }
-            }
-            override fun onAnimationCancel(animator: Animator?) {
-                currentAnimationStatus = ANIMATION_STATUS_IDLE
-            }
-            override fun onAnimationRepeat(animator: Animator?) {
-                currentAnimationStatus = if (animateIn) ANIMATION_STATUS_ANIMATING_IN else ANIMATION_STATUS_ANIMATING_OUT
-            }
-        })
-        animator.start()
     }
 }
